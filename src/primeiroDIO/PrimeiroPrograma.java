6705652ea33d0f4ff5718fe8212b5654c30215cd
Jornada <mauriciojornada@yahoo.com.br>
2021-07-21 11:33:50 -0400

    first commit

---
+++ src/primeiroDIO/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package primeiroDIO;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		/*int a = 2;
+		int b = 3;
+		System.out.println("Hello World! " + (a+b));*/
+		
+		segundoDIO segundoDIO = new segundoDIO(null, null, null);
+	}
+
+}
